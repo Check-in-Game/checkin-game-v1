1d44f9dd8bf204bf4ea9951f34f4e43baf9ac51c
Jokin <327928971@qq.com>
2021-09-03 11:07:27 +0800

    :rabbit2: Local version

--- index.php
+++ index.php
@@ -23,9 +23,9 @@ $count = 1;
     <meta name="viewport" content="width=device-width,height=device-height,initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
     <link rel="shortcut icon" href="./icon.ico">
     <title>Check-in Game</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/checkin-static@1.0.12/assets/bootstrap/css/bootstrap.min.css">
-    <script src="https://cdn.jsdelivr.net/npm/jquery@3.4.1/dist/jquery.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/checkin-static@1.0.12/assets/bootstrap/js/bootstrap.bundle.min.js" charset="utf-8"></script>
+    <link rel="stylesheet" href="./assets/bootstrap.min.css">
+    <script src="./assets/jquery.min.js" charset="utf-8"></script>
+    <script src="./assets/bootstrap.bundle.min.js" charset="utf-8"></script>
   </head>
   <body>
     <!-- 幕布 -->
@@ -37,19 +37,13 @@ $count = 1;
         <p>还在等什么？点击下方的按钮下载客户端加入吧！</p>
         <p class="lead">
           <a class="btn btn-success" href="./register.html" target="_blank" role="button">注册账户</a>
-          <a class="btn btn-primary" href="./web_checkin.html" target="_self" role="button">在线端</a>
-          <a class="btn btn-info" href="https://jq.qq.com/?_wv=1027&k=5ax4j23" target="_blank" role="button">加入交流QQ群：887304185</a>
-        </p>
+          <a class="btn btn-primary" href="./web_checkin.html" target="_self" role="button">在线端</a>        </p>
         <p>
           客户端最新版本：<img src="https://img.shields.io/github/release/jokin1999/tcapps-checkin.svg?style=flat-square" alt="badge">
         </p>
       </div>
     </div>
     <div class="container">
-
-      <div class="alert alert-success" role="alert">
-        本站仅为怀旧服，数据不互通，限于服务器性能，每日如计算量过大将自动停服，次日开启。
-      </div>
 
       <!-- 更新 -->
 
@@ -110,16 +104,6 @@ $count = 1;
       <div class="alert alert-dark" role="alert">
         <h4 class="alert-heading">签到技巧</h4>
         签到运行时，请确保网络环境稳定，签到程序在签到失败后会再重试3次，全部失败后程序将不再重试，需要玩家手动再次点击启动。
-      </div>
-
-      <div class="alert alert-warning" role="alert">
-        <h4 class="alert-heading">版本说明</h4>
-        此版本仅为怀旧服，与其他服务器数据不互通。
-      </div>
-
-      <div class="alert alert-primary" role="alert">
-        <h4 class="alert-heading">联系我们</h4>
-          官方QQ群：887304185
       </div>
 
     </div>
